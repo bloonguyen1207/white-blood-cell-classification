716358f187526793d8b6f506ae369a3defa65263
Bloo Nguyen <bloonguyen1207@gmail.com>
2019-10-25 12:48:28 +0200

    Update VGG16

--- vgg16.py
+++ vgg16.py
@@ -6,8 +6,10 @@ import matplotlib.pyplot as plt
 import tensorflow.keras.backend as K
 
 from data import create_data, separate_features_and_label
+
+from tensorflow.keras.applications import VGG16
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, GlobalAveragePooling2D
 from tensorflow.keras.utils import to_categorical
 
 from tensorflow.keras import optimizers
@@ -53,13 +55,21 @@ model.add(Dense(units=4096, activation="relu"))
 model.add(Dense(units=4096, activation="relu"))
 model.add(Dense(units=2, activation="softmax"))
 
+VGG16_MODEL = VGG16(input_shape=(train_X.shape[1:]), include_top=False, weights=None)
+
+VGG16_MODEL.trainable = False
+global_average_layer = GlobalAveragePooling2D()
+prediction_layer = Dense(4, activation='softmax')
+
+model = Sequential([VGG16_MODEL, global_average_layer, prediction_layer])
+
 model.summary()
 
 adam = optimizers.Adam(lr=0.001)
-model.compile(optimizer=adam, loss="categorical_crossentropy", metrics=["accuracy"])
+model.compile(optimizer=adam, loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
-hist = model.fit(train_X, train_y, epochs=50, batch_size=128, validation_split=0.2)
-test_loss, test_acc = model.evaluate(test_X, test_y)
+hist = model.fit(train_X, categorical_train_y, epochs=50, steps_per_epoch=2, batch_size=128, validation_steps=2, validation_split=0.2)
+test_loss, test_acc = model.evaluate(test_X, categorical_test_y)
 
 # train and validation loss
 plt.plot(hist.history['loss'])
