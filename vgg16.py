626b584f3af4189ca1886ce2d949f28453038666
Bloo Nguyen <bloonguyen1207@gmail.com>
2019-10-25 11:49:07 +0200

    Update vgg16 input shape

--- vgg16.py
+++ vgg16.py
@@ -24,7 +24,7 @@ categorical_test_y = to_categorical(test_y)
 
 model = Sequential()
 
-model.add(Conv2D(input_shape=(224, 224, 3), filters=64, kernel_size=(3, 3), padding="same", activation="relu"))
+model.add(Conv2D(input_shape=(train_X.shape[1:]), filters=64, kernel_size=(3, 3), padding="same", activation="relu"))
 model.add(Conv2D(filters=64, kernel_size=(3, 3), padding="same", activation="relu"))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
